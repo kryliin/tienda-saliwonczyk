64eee16959abe521c21d2f27b294590255711135
saliwonczykc <saliwonczykc@frcu.utn.edu.ar>
2022-03-09 23:24:06 -0300

    navbar

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Navbar } from 'react-bootstrap';
+import './App.css';
+
+
+function App() {
+
+
+  return (
+    <div className="e-Commerce Saliwonczyk">
+      <header className="App-header">
+          <Navbar/>
+      </header>
+    </div>
+  );
+}
+
+export default App;
