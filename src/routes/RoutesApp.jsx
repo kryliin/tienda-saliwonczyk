e935353d8a28f22042587177aa8a86611bb2b67c
saliwonczykc <saliwonczykc@frcu.utn.edu.ar>
2022-03-18 17:55:25 -0300

    cambios para chackout

--- src/routes/RoutesApp.jsx
+++ src/routes/RoutesApp.jsx
@@ -1,18 +1,22 @@
 import NavBar from '../components/NavBar/NavBar'
 import Titulo from '../components/Titulo/Titulo'
 import ItemListContainer from '../container/ItemListContainer/ItemListConteiner'
+import Count from '../container/ItemListContainer/ItemCount/ItemCount.js'
 
 function RoutesApp() {
 
     return (
         <>
-            <NavBar/>          
-            <Titulo tituloProps='Alquileres Saliwonczyk'/>
+            <NavBar />
+            <Titulo tituloProps='Alquileres Saliwonczyk' />
 
-            <ItemListContainer 
-                titulo= { Titulo }
-                greeting='Bienvenidos a la Primer Tienda de Alquiler de Juegos de Mesa de la Ciudad' 
-               
+            <ItemListContainer
+                titulo={Titulo}
+                greeting='Bienvenidos a la Primer Tienda de Alquiler de Juegos de Mesa de la Ciudad'
+
+            />
+            <Count
+                stock={12}
             />
         </>
     )
